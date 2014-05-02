56932815895dee063ebeb53064a083ee9edf62ba
chafey <chafey@lury.net>
2014-05-02 10:35:29 -0500

    new changes

---
+++ src/image_data_service/cpu_time.h
@@ -0,0 +1,17 @@
+//
+//  cpu_time.h
+//  cornerstoneVisualizationService
+//
+//  Copyright (c) 2014 Chris Hafey
+//
+
+#ifndef __cornerstoneVisualizationService__cpu_time__
+#define __cornerstoneVisualizationService__cpu_time__
+
+
+
+double getCPUTime();
+
+
+#endif /* defined(__cornerstoneVisualizationService__cpu_time__) */
+
---
+++ src/image_data_service/process_info.h
@@ -0,0 +1,15 @@
+//
+//  process_info.h
+//  cornerstoneVisualizationService
+//
+//  Copyright (c) 2014 Chris Hafey
+//
+
+#ifndef __cornerstoneVisualizationService__process_info__
+#define __cornerstoneVisualizationService__process_info__
+
+    size_t getPeakRSS( );
+    size_t getCurrentRSS( );
+
+#endif /* defined(__cornerstoneVisualizationService__process_info__) */
+
